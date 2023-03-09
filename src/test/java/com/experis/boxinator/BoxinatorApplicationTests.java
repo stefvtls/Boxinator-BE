acdac827fd48be78e3b52f0ffe95cb09e9a3baca
stefvtls <stefvtls@gmail.com>
2023-03-09 08:39:21 +0100

    FEAT: initialized empty project template with appropriate dependencies and initial configuration

---
+++ src/test/java/com/experis/boxinator/BoxinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.boxinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
