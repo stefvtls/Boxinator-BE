c5cc7c59adfab0a7cb9b27d40fdc9b2cd0c894f2
stefvtls <stefvtls@gmail.com>
2023-03-13 09:03:28 +0100

    FEAT: addeed role-based protection based on JWT realm roles extraction

--- src/main/java/com/experis/boxinator/config/SecurityConfig.java
+++ src/main/java/com/experis/boxinator/config/SecurityConfig.java
@@ -12,30 +12,58 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+
+
+
+    // this section permits all, ALL ROUTES PUBLICLY ACCESSIBLE, for early stage of development only
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpsec) throws Exception {
-        httpsec.cors().and()
+        httpsec
+                .cors()
+                .and()
                 .sessionManagement().disable()
                 .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
-                        // define mapping
-
-                        // routes publicly accessible
+                                // routes publicly accessible
                                 .anyRequest().permitAll()
-//                        .requestMatchers("/api/v1/resources/public").permitAll()
-
-                        // routes that require admin permissions - .hasRole() is a nightmare with little-lemur!!!!
-//                        .requestMatchers("/api/v1/resources/authorized").hasAuthority("SCOPE_profile")
-
-                        // routes that to be logged in (on default all of them) - for now uncommented, because for testing is annoying to add new token every five minutes:
-//                        .anyRequest().authenticated()
-
                 ).oauth2ResourceServer()
                 .jwt();
-//                .jwtAuthenticationConverter(jwtConverter());
         return httpsec.build();
     }
 
+
+
+
+
+
+    // READY SOLUTION FOR PRODUCTION WITH PROTECTION OF THE ENDPOINTS, UNCOMMENT WHOLE SECTION BELOW (two beans).
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpsec) throws Exception {
+//        httpsec
+//                .cors()
+//                .and()
+//                .sessionManagement().disable()
+//                .csrf().disable()
+//                .authorizeHttpRequests(authorize -> authorize
+//                        // define mapping
+//
+//                        // routes publicly accessible
+////                                .anyRequest().permitAll()
+//                        .requestMatchers("/api/v1/resources/public").permitAll()
+//
+//                        // routes that require admin permissions
+//                        .requestMatchers("/api/v1/resources/authorized").hasRole("ADMIN")
+//
+//                        // routes that to be logged in (on default all of them) - for now uncommented, because for testing is annoying to add new token every five minutes:
+//                        .anyRequest().authenticated()
+//
+//                ).oauth2ResourceServer()
+//                .jwt()
+//                .jwtAuthenticationConverter(jwtConverter());
+//        return httpsec.build();
+//    }
+//
 //    @Bean
 //    public JwtAuthenticationConverter jwtConverter() {
 //        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
